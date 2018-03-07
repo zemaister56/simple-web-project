d3f261c652856fc19f85559a71bb6e4fae4aae9f
Václav Chalupa <vac.chalupa@gmail.com>
2018-03-07 09:35:06 +0100

    Create & init blog repository

--- src/main/java/cz/sosjh/web/SimpleWebApplication.java
+++ src/main/java/cz/sosjh/web/SimpleWebApplication.java
@@ -1,12 +1,32 @@
 package cz.sosjh.web;
 
+import cz.sosjh.web.model.BlogEntry;
+import cz.sosjh.web.repository.BlogRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class SimpleWebApplication {
 
     public static void main(String[] args) {
+        initRepository();
         SpringApplication.run(SimpleWebApplication.class, args);
+    }
+
+    private static void initRepository() {
+        BlogRepository.add(new BlogEntry(
+                "Title #1",
+                "Author #1",
+                LocalDateTime.now().minusDays(2),
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ));
+        BlogRepository.add(new BlogEntry(
+                "Title #2",
+                "Author #2",
+                LocalDateTime.now().minusDays(1),
+                "Duis vitae arcu sed tellus efficitur placerat in sed tellus."
+        ));
     }
 }
---
+++ src/main/java/cz/sosjh/web/repository/BlogRepository.java
@@ -0,0 +1,23 @@
+package cz.sosjh.web.repository;
+
+import cz.sosjh.web.model.BlogEntry;
+
+import java.util.Collections;
+import java.util.List;
+
+public final class BlogRepository {
+
+    private BlogRepository() {
+        // static repository, do not initialize
+    }
+
+    private static List<BlogEntry> entries;
+
+    public static void add(BlogEntry entry) {
+        entries.add(entry);
+    }
+
+    public static List<BlogEntry> getAll() {
+        return Collections.unmodifiableList(entries);
+    }
+}
